09a160157bdef1213477637531365114e61d8063
Van Trong Hung 20173158 <hung.vt173158@sis.hust.edu.vn>
2022-03-22 09:21:32 +0700

    apiV1 permission group

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 import env from "./env";
 
 async function connect() {
-    try {
-        await mongoose.connect(env.MONGO_DB);
-        console.log("Connect OK");
-    } catch (e) {
-        console.log("Connect NOT OK");
-    }
+    await mongoose.connect(env.MONGO_DB);
+    console.log("Connect OK");
 }
 export default connect;
